e48c0ed5756cc220c1e28629be137269ab8bb647
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2015-06-08 23:31:23 +0900

    allow only json for cluster controller

--- app/controllers/clusters_controller.rb
+++ app/controllers/clusters_controller.rb
@@ -1,7 +1,50 @@
-class ClustersController < InheritedResources::Base
+class ClustersController < ApplicationController
+  before_action :set_project
+  before_action :set_cluster, only: [:show, :edit, :update, :destroy]
+
+  # GET /clusters.json
+  def index
+    @clusters = @project.clusters.includes(:elements)
+  end
+
+  # GET /clusters/1.json
+  def show
+  end
+
+  # POST /clusters.json
+  def create
+    @cluster = Cluster.new(cluster_params)
+
+    if @cluster.save
+      render :show, status: :created, location: @cluster
+    else
+      render json: @cluster.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /clusters/1.json
+  def update
+    if @cluster.update(cluster_params)
+      render :show, status: :ok, location: @cluster
+    else
+      render json: @cluster.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /clusters/1.json
+  def destroy
+    @cluster.destroy
+    head :no_content
+  end
 
   private
 
+    # Use callbacks to share common setup or constraints between actions.
+    def set_project
+      @project = Project.includes(:clusters).find(params[:project_id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
     def cluster_params
       params.require(:cluster).permit(:name, :project_id)
     end
